57dad43997fe138a8c9091fb5ce41e452016ff35
SharuHaru <harveygonzaga222@gmail.com>
2023-05-31 23:40:03 +0800

    practice dom maniuplulation

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,13 +1,28 @@
 //  =============== VARIABLES ===============
 const clickButton = document.querySelector('.button');
+const listNav = document.querySelector(".link-div ul");
+console.log(listNav);
 
 
-function showForm(){
-    const numberOtp = document.querySelector('.number-otp');
-    numberOtp.style.display = 'flex';
-    
 
+function navHover(){
+
+        const liNav = document.querySelectorAll(".link-div li");
+
+        for (let i = 0; i < 5; i++) {
+            liNav[i].style.color = 'blue';
+        }
+        
+
+   
 }
 
-clickButton.addEventListener('click', showForm);
+// function showForm(){
+//     const numberOtp = document.querySelector('.number-otp');
+//     numberOtp.style.display = 'flex';
+    
+
+// }
+
+// clickButton.addEventListener('click', showForm);
 
