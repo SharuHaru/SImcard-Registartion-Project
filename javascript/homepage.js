4428de8793bbf0ccfc2fff4234f5c2067328ccb5
SharuHaru <harveygonzaga222@gmail.com>
2023-06-01 23:42:06 +0800

    add event listener to navbar
    text in navbar changes color
    navigate to home and to faqs

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,28 +1,46 @@
-//  =============== VARIABLES ===============
-const clickButton = document.querySelector('.button');
-const listNav = document.querySelector(".link-div ul");
-console.log(listNav);
+// =================== NAVIGATION JAVASCRIPT ===================
 
+//Hover Effect
 
+const navli = document.querySelector('.link-div ul');
+console.log(navli);
 
-function navHover(){
+navli.addEventListener('mouseover', function(e) {
+    console.log(e);
+    const target = e.target;
 
-        const liNav = document.querySelectorAll(".link-div li");
-
-        for (let i = 0; i < 5; i++) {
-            liNav[i].style.color = 'blue';
-        }
+    if(target.matches('li') && !target.classList.contains('active-li')){
+        target.style.color = 'rgba(5, 0, 255, 0.5)';
         
+    }
+});
 
-   
-}
+navli.addEventListener('mouseout', function (e) {
+        console.log(e);
+        const target = e.target;
 
-// function showForm(){
-//     const numberOtp = document.querySelector('.number-otp');
-//     numberOtp.style.display = 'flex';
+        if (target.matches('li') && !target.classList.contains('active-li')) {
+            target.style.color = 'inherit';
+        }
+    });
+
+
+
+// Click Effect 
+
+const faq = document.querySelector('#faqs');
+const home = document.querySelector('#home');
+
+faq.addEventListener('click', function(e) {
+    const faqContainer = document.querySelector('.faq-container');
+    faqContainer.scrollIntoView({ behavior: 'smooth' });
     
+    faq.classList.add('active-li');
+    home.classList.remove('active-li');
+    faq.style.color = 'rgba(9, 156, 242, 1)';
+});
 
-// }
-
-// clickButton.addEventListener('click', showForm);
+home.addEventListener('click', function() {
+    window.location.replace("./index.html");
+});
 
