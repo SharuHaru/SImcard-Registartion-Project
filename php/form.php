f0b2239adad50384d0fe1486e0cede8d8bdb58b1
SharuHaru <harveygonzaga222@gmail.com>
2023-06-09 02:09:08 +0800

    update

--- php/form.php
+++ php/form.php
@@ -1,23 +1,38 @@
 <?php
+
 include 'config.php';
 
-if(isset($_POST['submit'])){
-    $fname = $POST['fname'];
-    $mname = $_POST['mname'];
-    $lname = $_POST['lname'];
-    $date = $_POST['date'];
-    $gender = $_POST['gender'];
-    $password= ['password'];
+$mobile = $_POST['MobileNumber'];
+$firstName = $_POST['fname'];
+$midName = $_POST['mname'];
+$lastName = $_POST['lname'];
+$dateofBirth = $_POST['date'];
+$gender = $_POST['gender'];
+$password = $_POST['password'];
+$country = $_POST['password'];
+$province =
+$barangay =
+$city =
+$altcon =
+$email =
 
-    $sql = "INSERT INTO `profileinfotbl` (`FirstName`, `MiddleName`, `LastName`, `Gender`, `Password`) VALUES ($fname, $mname, '$lname, $date, $gender, $password)";
-    $result - mysqli_query($link, $sql);
 
-    if($result){
-        echo "data inserted successfully";
-    } else {
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }
-}
- 
+$stmt = $link->prepare("insert into userstbl(MobileNumber, FirstName, MiddleName, LastName, DateofBirth, Gender, Password) values(?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("issssss",$mobile, $firstName, $midName, $lastName, $dateofBirth, $gender, $password);
+$stmt->execute();
+echo"registartion successfully";
+$stmt->close();
 
+
+
+// if($conn->connect_error) {
+//     die('FAILED:' .$conn->connect_error);
+// } else {
+//     $meow = $conn->prepare("insert into profileinfotsbl(FirstName, MiddleName, LastName, DateofBirth, Gender, Password) values(?, ?, ?, ?, ?, ?)");
+//     $meow->bind_param("sss:datess",$firstName, $midName, $lastName. $date, $gender, $password);
+//     $meow->execute();
+//     echo"registartion successfully";
+//     $->close();
+//     $conn->close();
+// }
 ?>
