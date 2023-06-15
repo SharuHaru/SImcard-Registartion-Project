534db38046f26e1815aa38b29df9a20abfbe0033
SharuHaru <harveygonzaga222@gmail.com>
2023-06-15 15:14:14 +0800

    form js pop-up

--- javascript/form.js
+++ javascript/form.js
@@ -3,3 +3,12 @@ const cancel = document.querySelector('.cancel');
 cancel.addEventListener('click', function() {
     window.close();
 });
+
+const verify = document.querySelector('.verify');
+verify.addEventListener('click', function() {
+    const numberdiv = document.querySelector('.number');
+    numberdiv.style.display = 'none';
+
+    const body = document.body;
+    body.style.backgroundColor = '#E9F1FA';
+});
