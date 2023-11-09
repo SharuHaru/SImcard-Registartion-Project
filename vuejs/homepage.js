5d38da081b1c99d91638d877e4ea9817b2f3be0b
SharuHaru <harveygonzaga222@gmail.com>
2023-11-09 17:02:58 +0800

    NavBar Data

--- vuejs/homepage.js
+++ vuejs/homepage.js
@@ -1,7 +1,18 @@
 Vue.createApp({
     data() {
         return {
+            title : 'Sim Registration',
+            menus : [
+                {name: 'HOME', url: './index.html'},
+                {name: 'ABOUT', url: '/about-us.html'},
+                {name: 'ACCOUNT', url: '/account.html'},
+                {name: 'CONTACT US', url: '/about-us.html'},
+                {name: 'FAQs', url: '/about-us.html'}
+
+            ],
+            carouselTitle : 'Register Your Sim Now!',
+            carouselBody : 'Republic Act No. 11934 and commonly referred to as the SIM card law, is a Philippine law mandating the registration of SIM cards before activation.'
             
-        }
+        };
     }
-}).mount(body);
+}).mount('body');
